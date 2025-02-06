09ce62ddffbbf253fba01dc69b76ecfe1634f00d
Krythom <krythomia@gmail.com>
2025-02-06 11:53:30 -0400

    removed _checkSet as it was no longer needed

--- CAT/Iterators/StrengthMutation.cs
+++ CAT/Iterators/StrengthMutation.cs
@@ -7,14 +7,17 @@ public class StrengthMutation(int types) : Iterator
 {
     private FloatCell[,] _world;
     private FloatCell[,] _newWorld;
-    List<FloatCell> _neighbors = [];
+    private List<FloatCell> _neighbors = [];
     private readonly Color[] _colors = new Color[types];
-    private readonly HashSet<Point> _checkSet = [];
+    private int _width;
+    private int _height;
         
     public override FloatCell[,] InitWorld(int width, int height)
     {
         _world = new FloatCell[width, height];
         _newWorld = new FloatCell[width, height];
+        _width = width;
+        _height = height;
 
         for (int i = 0; i < types; i++)
         {
@@ -27,7 +30,6 @@ public class StrengthMutation(int types) : Iterator
             {
                 int id = Rand.Next(types);
                 _world[x,y] = new FloatCell(x, y, Rand.NextDouble(), id, _colors[id]);
-                _checkSet.Add(_world[x,y].Pos);
             }
         }
         
@@ -40,35 +42,38 @@ public class StrengthMutation(int types) : Iterator
         {
             _colors[i] = Mutate(_colors[i], 5);
         }
-        
-        foreach ((int x, int y) in _checkSet)
-        {
-            FloatCell current = _world[x, y];
-            _neighbors = current.GetMoore(_world, 1, true, _neighbors);
 
-            double highest = double.MinValue;
-            FloatCell winner = current;
-            
-            foreach (FloatCell neighbor in _neighbors)
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
             {
-                if (neighbor.Strength > highest)
+                FloatCell current = _world[x, y];
+                _neighbors = current.GetMoore(_world, 1, true, _neighbors);
+
+                double highest = double.MinValue;
+                FloatCell winner = current;
+            
+                foreach (FloatCell neighbor in _neighbors)
                 {
-                    highest = neighbor.Strength;
-                    winner = neighbor;
+                    if (neighbor.Strength > highest)
+                    {
+                        highest = neighbor.Strength;
+                        winner = neighbor;
+                    }
                 }
-            }
                 
-            if (winner.Id != current.Id)
-            {
-                _newWorld[x, y] = new FloatCell(x, y, highest, winner.Id, _colors[winner.Id]);
-            }
-            else
-            {
-                _newWorld[x, y] = new FloatCell(x, y, highest, winner.Id, current.Col);
-            }
+                if (winner.Id != current.Id)
+                {
+                    _newWorld[x, y] = new FloatCell(x, y, highest, winner.Id, _colors[winner.Id]);
+                }
+                else
+                {
+                    _newWorld[x, y] = new FloatCell(x, y, highest, winner.Id, current.Col);
+                }
             
-            _newWorld[x, y].Strength += Rand.NextDouble() - 1;
-        }
+                _newWorld[x, y].Strength += Rand.NextDouble() - 1;
+            }
+        } 
         
         (_newWorld, _world) = (_world, _newWorld);
         return _world;
